8278ab4bd41ead055fca15544e603ba013748c1d
IMGADMIN <IMGADMIN@DESKTOP-OST97I4>
2020-02-17 15:18:53 +0530

    changes for new apirequest

--- MCS.Test.Automation.Nunit/Tests/APIIntegration/HourlyWeatherReport.cs
+++ MCS.Test.Automation.Nunit/Tests/APIIntegration/HourlyWeatherReport.cs
@@ -11,14 +11,14 @@ using RestSharp;
 namespace MCS.Test.Automation.Tests.NUnit.Tests
 {
     [TestFixture]
-    public class CurrentWheatherUpdateAPIClass : ProjectTestBase
+    public class HourlyWeatherReport : ProjectTestBase
     {
 
         [Test]
-        public async Task CurrentWheatherUpdateAPITest()
+        public async Task HourlyWeatherReportTest()
         {
 
-            var client = new RestClient("https://samples.openweathermap.org/data/2.5/weather?id=2172797&appid=b6907d289e10d714a6e88b30761fae22");
+            var client = new RestClient("https://samples.openweathermap.org/data/2.5/forecast?id=524901&appid=b1b15e88fa797225412429c1c50c122a1");
             var request = new RestRequest(Method.GET);
             IRestResponse response = client.Execute(request);
         }
