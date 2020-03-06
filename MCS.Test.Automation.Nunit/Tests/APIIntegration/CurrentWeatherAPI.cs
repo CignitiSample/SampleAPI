9f38bf3232e4116af39572aad78aab8f97d148a8
IMGADMIN <IMGADMIN@DESKTOP-OST97I4>
2020-03-06 19:29:32 +0530

    added currentweather API

--- MCS.Test.Automation.Nunit/Tests/APIIntegration/CurrentWeatherAPI.cs
+++ MCS.Test.Automation.Nunit/Tests/APIIntegration/CurrentWeatherAPI.cs
@@ -11,11 +11,11 @@ using RestSharp;
 namespace MCS.Test.Automation.Tests.NUnit.Tests
 {
     [TestFixture]
-    public class CurrentWheatherUpdateAPIClass : ProjectTestBase
+    public class CurrentWeatherUpdateAPIClass : ProjectTestBase
     {
 
         [Test]
-        public async Task CurrentWheatherUpdateAPITest()
+        public async Task CurrentWeatherUpdateAPITest()
         {
 
             var client = new RestClient("https://samples.openweathermap.org/data/2.5/weather?id=2172797&appid=b6907d289e10d714a6e88b30761fae22");
