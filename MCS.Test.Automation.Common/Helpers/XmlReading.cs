0b287224d761b98221a746049d59dd4e8e388a96
IMGADMIN <IMGADMIN@DESKTOP-OST97I4>
2020-03-19 15:20:35 +0530

    committed current weather report API

---
+++ MCS.Test.Automation.Common/Helpers/XmlReading.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace MCS.Test.Automation.Common.Helpers
+{
+    public static class XmlReading
+    {
+        public const string CurrentWEeatherUrl = "id=2172797&appid=b6907d289e10d714a6e88b30761fae22";
+        //public static string GetUrl(string xmlPath,string tesName)
+        //{
+        //    string urlValue = string.Empty;
+        //    XmlDocument doc = new XmlDocument();
+        //    doc.Load(xmlPath);
+        //   XmlNode node =  doc.SelectSingleNode("[/tests/AddOrderApiPayloadTest]");
+        //    return urlValue;
+        //}
+    }
+}
--- MCS.Test.Automation.Nunit/Tests/APIIntegration/CurrentWeatherAPI.cs
+++ MCS.Test.Automation.Nunit/Tests/APIIntegration/CurrentWeatherAPI.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Net.Http;
 using System.Threading.Tasks;
+using System.Web.Script.Serialization;
 using MCS.Test.Automation.Common;
 using MCS.Test.Automation.Common.Helpers;
 using MCS.Test.Automation.Tests.NUnit.DataDriven;
+using Newtonsoft.Json.Linq;
 using NUnit.Framework;
+using RelevantCodes.ExtentReports;
 using RestSharp;
 
 namespace MCS.Test.Automation.Tests.NUnit.Tests
@@ -13,14 +17,45 @@ namespace MCS.Test.Automation.Tests.NUnit.Tests
     [TestFixture]
     public class CurrentWeatherUpdateAPIClass : ProjectTestBase
     {
+        public static ExtentTest ExtentStepTest { get; set; }
+
+        private static ExtentReports extent;
 
         [Test]
         public async Task CurrentWeatherUpdateAPITest()
         {
+           extent = new ExtentReports("D:\\Reports\\Report.html", true);
+            ExtentStepTest = extent.StartTest("TestName");
+            ExtentStepTest.Log(LogStatus.Info,"Test started");
+            string url = ConfigurationManager.AppSettings["BaseUrl"] + XmlReading.CurrentWEeatherUrl;
+            //var client = new RestClient("https://samples.openweathermap.org/data/2.5/weather?id=2172797&appid=b6907d289e10d714a6e88b30761fae22");
+          
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = null;
+            response = client.GetAsync(url).Result;
+            string responseString = await response.Content.ReadAsStringAsync();
 
-            var client = new RestClient("https://samples.openweathermap.org/data/2.5/weather?id=2172797&appid=b6907d289e10d714a6e88b30761fae22");
-            var request = new RestRequest(Method.GET);
-            IRestResponse response = client.Execute(request);
+            //JavaScriptSerializer serializer = new JavaScriptSerializer();
+
+            //dynamic jsonObject = serializer.Deserialize<dynamic>(responseString);
+
+
+            //dynamic weatherdata = JObject.Parse(responseString);
+            if (response.IsSuccessStatusCode)
+                    {
+                ExtentStepTest.Log(LogStatus.Pass, "Received response ", "Response: " + responseString + "\n" + "HTTP Status: " + response.StatusCode + " Reason :" + response.ReasonPhrase);
+
+                //ExtentStepTest.Log(LogStatus.Pass, "Received response ", "Response: " + responseString + "\n" );
+                //ExtentStepTest.Log(LogStatus.Pass, "Weather Report ", "Response: " + jsonObject.Weather + "\n");
+                //ExtentStepTest.Log(LogStatus.Pass, "HTTP Status: " + response.StatusCode + " Reason :" + response.ReasonPhrase + "\n");
+            }
+                    else
+                    {
+                        ExtentStepTest.Log(LogStatus.Fail, "Received response as API validation failed, Please try to reprocess with correct information", "Response: " + responseString + "\n" + "HTTP Status: " + response.StatusCode + " Reason :" + response.ReasonPhrase);
+                    }
+            extent.EndTest(ExtentStepTest);
+            extent.Flush();
         }
     }
 }
+
